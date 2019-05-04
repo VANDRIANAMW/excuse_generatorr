672b9913240a70013be76e4cf2f9d23f5158a71c
VANDRIANAMW <VANDRIANAMW@gmail.com>
2019-05-04 17:46:53 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use stric'
+let who  = ['Yo','El metro','Mi mamá','Los profesores'];
+let what = ['no escuche la alarma','se quedo sin servicio','no me dejo salir de casa','cerraron las vias'];
+let when = ['esta mañana','en la linea 1','en todo el dia','cuando venia en camino'];
+
+let index_one =Math.floor(Math.random()*who.length);
+let index_two =Math.floor(Math.random()*what.length);
+let index_three =Math.floor(Math.random()*when.length);
+
+
+
+
+document.getElementById("excusas").innerHTML = who[index_one]+ " "+what[index_two]+" "+when[index_three];
